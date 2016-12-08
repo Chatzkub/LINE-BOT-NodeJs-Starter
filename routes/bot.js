c1c585adc6005bfd88e10179817a49090155cbc2
Mac mini 2015 <macmini@Mac-mini-Chat.local>
2016-12-08 15:14:17 +0700

    update

---
+++ routes/bot.js
@@ -0,0 +1,10 @@
+var test = "in bot js";
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(test);
+});
+
+module.exports = router;
