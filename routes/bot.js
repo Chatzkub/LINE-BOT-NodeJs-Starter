deb7e3d8747eeb839dd865ad69f07c623f962be3
Mac mini 2015 <macmini@Mac-mini-Chat.local>
2016-12-16 11:58:53 +0700

    Update

--- routes/bot.js
+++ routes/bot.js
@@ -148,7 +148,7 @@ router.post('/',function(req, res){
               console.log("########photo_file######" + formData.photo_file);
 
 
-              request.post({url:'http://console.selfiprint.com/api/1.0/uploadPhoto', formData: formData, headers: headers}, function optionalCallback(err, httpResponse, body) {
+              request.post({url:'http://console.selfiprint.com/api/1.0/uploadPhoto', formData: formData, headers: headers ,timeout: 1000000}, function optionalCallback(err, httpResponse, body) {
                 if (err) {
                   return console.error('upload failed:', err);
                 }
